62853756427e1d3c547740b9ec7594894d3313b3
Ripon <ripon18922@gmail.com>
2023-08-27 04:34:36 +0600

    Payment button is created without token

--- utilities.js
+++ utilities.js
@@ -1,20 +1,9 @@
 document.getElementById('btn-apply').addEventListener('click',function(){
     const previousPrice=document.getElementById('price');
     const payAble=previousPrice.innerText;
-    const getToken=document.getElementById('token');
-    const trueToken=getToken.value;
-
-    if(trueToken==='DISCO'){
-        const payAbleTotal=(payAble*30)/100;
-        const haveToPay=payAble-payAbleTotal;
+    const payAbleTotal=(payAble*30)/100;
+    const haveToPay=payAble-payAbleTotal;
    document.getElementById('pay-total').innerHTML=haveToPay;
-    }
-    else{
-        console.log(payAble);
-        document.getElementById('pay-total').innerHTML=payAble;
-    }
-    
-    
 })
 
 
